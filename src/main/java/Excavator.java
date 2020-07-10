7c909f48734697b92304081df876bd366d91b3cd
pavelfromua <50228485+pavelfromua@users.noreply.github.com>
2020-07-10 13:07:39 +0300

    result

---
+++ src/main/java/Excavator.java
@@ -0,0 +1,11 @@
+public class Excavator extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("The Excavator is started to work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("The Excavator is stopped");
+    }
+}
---
+++ src/main/java/MainApp.java
@@ -0,0 +1,22 @@
+/**
+ * - Создать класс Machine с методами public abstract void doWork()
+ * и public abstract void stopWork().
+ * - Создать классы Track, Bulldozer, Excavator которые унаследуют Machine.
+ * - Переопределить метод doWork() и вывести в консоль строку с сообщением что та или иная
+ * машина начала свою работу.
+ * - Переопределить метод stopWork() и вывести в консоль строку с сообщением что та или иная
+ * машина прекратила свою работу.
+ * - В классе Main создать добавить по одному екземпляру каждого класса.
+ * - Отправить все машини на работу и остановить работу всех машин.
+ */
+
+public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machines = {new Track(), new Bulldozer(), new Excavator()};
+
+        for (Machine machine: machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
+}
